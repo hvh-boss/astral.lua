f7c537b6181b5730d5538717d03a625d10c4f8e7
Thomas Clayton <bryanoli.savas@gmail.com>
2025-09-07 22:12:34 +0800

    Update Universal.lua

--- Universal/Assets/Universal.lua
+++ Universal/Assets/Universal.lua
@@ -8,6 +8,7 @@ IsVisible:: function(startPos: Vector3, toPart: Model): boolean
 EspPositionType:: function() <-- Dont really need to overwrite this function
 DrawLine:: function(StartPos: Number, EndPos: Number, Thick: Number, Transparency: Number): Line
 Convert3DTo2D:: function(pos: Vector3): Vector2
-SetCamPos:: function(pos: Vector3, smoothness: Number)
+SetCamPos:: function(pos: Vector3, smoothness: Number) <- sets camera position ( this is for aimbot )
 ClosestToMouseRadius:: function(teamcheck: Boolean, wallcheck: Boolean, radius: Radius): Model
+mouse1click:: function() <- This is for triggerbot basically triggers mouse1click you can change the function for like shoot functions
 ]]
